9b4da91cbea6d687d5fd9a6c89f1db2412a30ced
YoussefHanna64-bit <JCBIOX200@GMAIL.COM>
2026-05-28 06:36:29 +0300

    Feat: set up server with database connection and error handling; add httpStatus utility

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,31 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/dbConfig.js";
+import httpStatus from "./utils/httpStatus.js";
 import cors from "cors";
 
+dotenv.config();
+connectDB();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res) => {
+  return res.status(404).json({
+    status: httpStatus.FAIL,
+    message: "Route not found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    status: err.status || httpStatus.ERROR,
+    message: err.message || "Internal Server Error",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running");
+});
