cc9bb7decd53b8b81631a02dc6d25f1ae50d056a
YoussefHanna64-bit <JCBIOX200@GMAIL.COM>
2026-06-09 11:43:14 +0300

    Feat: integrate CategoryCubit with CategoryFilter for category loading with Skeletonizer

--- Mobile/lib/features/search/presentation/widgets/filter.dart
+++ Mobile/lib/features/search/presentation/widgets/filter.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:nexora/core/models/category_model.dart';
 import 'package:nexora/core/theme/text_styles.dart';
 import 'package:nexora/core/widgets/custom_bottom_sheet_container.dart';
 import 'package:nexora/core/widgets/custom_primary_button.dart';
+import 'package:nexora/features/home/presentation/manager/category_cubit/category_cubit.dart';
+import 'package:nexora/features/home/presentation/manager/category_cubit/category_state.dart';
 import 'package:nexora/features/search/presentation/widgets/category_filter_wrap.dart';
 import 'package:nexora/features/search/presentation/widgets/price_range_slider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class Filter extends StatefulWidget {
   const Filter({super.key});
@@ -63,19 +67,48 @@ class _FilterState extends State<Filter> {
           Text(l10n.categories,
               style: AppTextStyles.bold14Black.copyWith(color: onSurface)),
           SizedBox(height: h * 0.02),
-          // CategoryFilterWrap(
-          //   selectedCategories: selectedCategories,
-          //   categories: dummyCategories,
-          //   onCategorySelected: (category, isSelected) {
-          //     setState(() {
-          //       if (isSelected) {
-          //         selectedCategories.add(category);
-          //       } else {
-          //         selectedCategories.remove(category);
-          //       }
-          //     });
-          //   },
-          // ),
+          BlocBuilder<CategoryCubit, CategoryState>(
+            builder: (context, state) {
+              if (state is CategoryError) {
+                return Text(state.message,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: Colors.red));
+              }
+
+              final bool isLoading =
+                  state is CategoryLoading || state is CategoryInitial;
+
+              final List<Category> displayCategories = isLoading
+                  ? [
+                      Category(id: '1', name: 'Electronics', image: ''),
+                      Category(id: '2', name: 'Smartphones', image: ''),
+                      Category(id: '3', name: 'Laptops', image: ''),
+                      Category(id: '4', name: 'Tablets', image: ''),
+                      Category(id: '5', name: 'Accessories', image: ''),
+                    ]
+                  : (state as CategorySuccess).categories;
+
+              return Skeletonizer(
+                enabled: isLoading,
+                child: CategoryFilterWrap(
+                  selectedCategories: selectedCategories,
+                  categories: displayCategories,
+                  onCategorySelected: isLoading
+                      ? (category, isSelected) {}
+                      : (category, isSelected) {
+                          setState(() {
+                            if (isSelected) {
+                              selectedCategories.clear();
+                              selectedCategories.add(category);
+                            } else {
+                              selectedCategories.remove(category);
+                            }
+                          });
+                        },
+                ),
+              );
+            },
+          ),
           SizedBox(height: h * 0.02),
           CustomPrimaryButton(
             buttonText: l10n.applyFilters,
