5288a38d39c1f3f3f6108e1528dc10513a57e05b
YoussefHanna64-bit <JCBIOX200@GMAIL.COM>
2026-06-12 18:51:00 +0300

    Feat: implement ApiProductRepoImpl and  ProductRepo

---
+++ Mobile/lib/features/product/data/repositories/api_product_repo_impl.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:nexora/core/errors/failure.dart';
+import 'package:nexora/core/models/product_model.dart';
+import 'package:nexora/core/network/api_service.dart';
+import 'package:nexora/core/network/end_points.dart';
+import 'package:nexora/features/product/domain/repositories/product_repo.dart';
+
+class ApiProductRepoImpl implements ProductRepo {
+  final ApiService apiService;
+
+  ApiProductRepoImpl(this.apiService);
+
+  @override
+  Future<Either<Failure, List<Product>>> getAllProducts() async {
+    try {
+      final response = await apiService.get(EndPoints.products);
+
+      List<Product> products = [];
+      for (var item in response.data['data']['products']) {
+        products.add(Product.fromJson(item));
+      }
+
+      return Right(products);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
